ef3476840c68ffe822c290c8412e82b0840d1a89
Anas CHAHID <anas.chahid777@gmail.com>
2021-11-18 21:25:27 +0100

    Initial commit

---
+++ app/src/main/java/com/watchcorn/watchcorn/MainActivity.java
@@ -0,0 +1,19 @@
+package com.watchcorn.watchcorn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
